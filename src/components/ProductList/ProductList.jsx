070ab4667711aeb9d6e5c9f028327ab097f9455a
start1810 <striganovartem@gmail.com>
2024-05-04 19:52:05 +0300

    update ProductList4

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -20,15 +20,14 @@ const getTotalPrice = (items = []) => {
 }
 
 const ProductList = () => {
-    const [addedItems, setAddedItems] = useState();
+    const [addedItems, setAddedItems] = useState([]);
     const {tg} = useTelegram();
-
     const onAdd = (product) => {
-        const alreadyAdded = addedItems.find((item) => item.id === product.id);
+        const alreadyAdded = addedItems?.find((item) => item?.id === product.id);
         let newItems = [];
 
         if (alreadyAdded) {
-            newItems = addedItems.filter(item => item.id !== product.id);
+            newItems = addedItems?.filter(item => item?.id !== product.id);
         } else {
             newItems = [...addedItems, product];
         }
