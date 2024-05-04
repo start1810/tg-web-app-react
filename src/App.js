dd3e359dc7980bbd2f5c81edde0acfce1f9ff8ce
start1810 <striganovartem@gmail.com>
2024-05-04 20:04:51 +0300

    update ProductList6

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Header from './components/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import ProductList from './components/ProductList/ProductList';
 import Form from './components/Form/Form';
+import Button from './components/Button/Button';
+import ProductItem from './components/ProductItem/ProductItem';
 import RunnerForm from './components/RunnerForm/RunnerForm';
 
 function App() {
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,40 +1,44 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
 import './ProductList.css';
-import ProductItem from "../../components/ProductItem/ProductItem";
+import ProductItem from "../ProductItem/ProductItem";
 import {useTelegram} from "../../hooks/useTelegram";
+import {useCallback, useEffect} from "react";
 
 const products = [
-    {id:1, title: 'T-shirt', price: 400, description: 'Run clothes'},
-    {id:2, title: 'Longsleave', price: 800, description: 'Cold Run clothes'},
-    {id:3, title: 'Hoodie', price: 9000, description: 'Warm Run clothes'},
-    {id:4, title: 'Sneakers', price: 12000, description: 'Run shoes'},
-    {id:5, title: 'T-shirt', price: 900, description: 'Run clothes'},
-    {id:6, title: 'Shirts', price: 400, description: 'Run clothes'},
-    {id:7, title: 'Bandana', price: 100, description: 'Head Run clothes'},
+    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
+    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
+    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
+    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
 ]
 
 const getTotalPrice = (items = []) => {
     return items.reduce((acc, item) => {
-        return acc + item.price;
+        return acc += item.price
     }, 0)
 }
 
 const ProductList = () => {
     const [addedItems, setAddedItems] = useState([]);
-    const {tg} = useTelegram();
+    const {tg, queryId} = useTelegram();
+
+
     const onAdd = (product) => {
-        const alreadyAdded = addedItems?.find((item) => item?.id === product.id);
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
         let newItems = [];
 
-        if (alreadyAdded) {
-            newItems = addedItems?.filter(item => item?.id !== product.id);
+        if(alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id);
         } else {
             newItems = [...addedItems, product];
         }
 
-        setAddedItems(newItems);
+        setAddedItems(newItems)
 
-        if (newItems.length === 0) {
+        if(newItems.length === 0) {
             tg.MainButton.hide();
         } else {
             tg.MainButton.show();
@@ -43,16 +47,16 @@ const ProductList = () => {
             })
         }
     }
+
     return (
-    
-        <div className={"list"}>
-            {products.map((item) => 
+        <div className={'list'}>
+            {products.map(item => (
                 <ProductItem
                     product={item}
                     onAdd={onAdd}
                     className={'item'}
-                />)
-            }
+                />
+            ))}
         </div>
     );
 };
