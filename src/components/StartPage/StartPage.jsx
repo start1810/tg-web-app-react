4dd28877475c499017ea61c868c67574a9e0b70d
start1810 <striganovartem@gmail.com>
2024-05-26 20:56:56 +0300

    updated calculator tempo7

--- src/components/StartPage/StartPage.jsx
+++ src/components/StartPage/StartPage.jsx
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button/Button";
 import { Link } from "react-router-dom";
 import "./StartPage.css"
 
 const StartPage = () => {
+    const [color, setColor] = useState('');
+
+    const onChangeColorBlue = () => {
+        setColor('colorblue')
+    }
+    const onChangeColorTg = () => {
+        setColor('colorTg')
+    }
     return (
         <div className="page">
             <div className="photo1"></div>
-            <div className="btn">
+            <div className="btn"
+                    onMouseOver={onChangeColorBlue}
+                    onMouseOut={onChangeColorTg}
+                    >
                 <Link to="/tempocalculatorform">
-                    <Button className={"menubtn"}>
-                    <table>
-                        <tr>
-                            <td>
-                            <h1>Калькулятор темпов</h1>
-                            <p>Подберите оптимальные темпы и сделайте тренировки продуктивнее</p>
-                            </td>
-                            <td className="symbolgo">
-                                {'>'}
-                            </td>
-                        </tr>
-                    </table>
+                    <Button 
+                        className={`menubtn ${color}`} 
+                        
+                        >
+                        <table className="btntable">
+                            <tr>
+                                <td>
+                                <h1>Калькулятор темпов</h1>
+                                <p>Ответьте на вопросы, подберите оптимальные темпы и сделайте тренировки продуктивнее</p>
+                                </td>
+                                <td className="symbolgo">
+                                    {'>'}
+                                </td>
+                            </tr>
+                        </table>
                     </Button>
                 </Link>
             </div>
