e7f8fd1d6891d6877389dac6f4b077d7f0bfa603
start1810 <striganovartem@gmail.com>
2024-05-26 17:44:35 +0300

    updated calculator tempo6

--- src/components/StartPage/StartPage.jsx
+++ src/components/StartPage/StartPage.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import Button from "../Button/Button";
 import { Link } from "react-router-dom";
+import "./StartPage.css"
 
 const StartPage = () => {
     return (
         <div>
+            <div className="logo"></div>
             <div>
                 <Link to="/tempocalculatorform">
-                    <Button>Калькулятор темпов</Button>
+                    <Button className={"menubtn"}>Калькулятор темпов</Button>
                 </Link>
             </div>
+            {/*
             <div>
                 <Link to="/runnerprogram">
                     <Button>Редактировать тренировочный план</Button>
                 </Link>
             </div>
+            */}
             
         </div>
     )
