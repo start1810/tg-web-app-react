cf88cec7590fd89dcf21ca4047c413ed648dbdea
start1810 <striganovartem@gmail.com>
2024-05-29 23:05:32 +0300

    updated calculator tempo7

--- src/components/TempoCalculatorResults/TempoCalculatorResults.jsx
+++ src/components/TempoCalculatorResults/TempoCalculatorResults.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation, Link} from "react-router-dom";
 import findNearTime from "../../scripts/tempoCalculator/findNearTime.js";
 import getLevel from "../../scripts/tempoCalculator/getLevel.js";
@@ -12,6 +12,54 @@ import dataValidation from "../../scripts/tempoCalculator/dataValidation.js";
 import countHRRzones from "../../scripts/tempoCalculator/countHRRzones.js";
 
 const TempoCalculatorResult = () => {
+    const [firstZone, setFirstZone] = useState(['hiden','borderTg']);
+    const [secondZone, setSecondZone] = useState(['hiden','borderTg']);
+    const [thirdZone, setThirdZone] = useState(['hiden','borderTg']);
+    const [fourthZone, setFourthZone] = useState(['hiden','borderTg']);
+    const onChoicedFirstZone = () => {
+        if (firstZone[0]==='hiden') {
+            setFirstZone(['','borderActive'])
+
+            setSecondZone(['hiden','borderTg'])
+            setThirdZone(['hiden','borderTg'])
+            setFourthZone(['hiden','borderTg'])
+        } else {
+            setFirstZone(['hiden','borderTg'])
+        }
+    }
+    const onChoicedSecondZone = () => {
+        if (secondZone[0]==='hiden') {
+            setSecondZone(['','borderActive'])
+
+            setFirstZone(['hiden','borderTg'])
+            setThirdZone(['hiden','borderTg'])
+            setFourthZone(['hiden','borderTg'])
+        } else {
+            setSecondZone(['hiden','borderTg'])
+        }
+    }
+    const onChoicedThirdZone = () => {
+        if (thirdZone[0]==='hiden') {
+            setThirdZone(['','borderActive'])
+
+            setFirstZone(['hiden','borderTg'])
+            setSecondZone(['hiden','borderTg'])
+            setFourthZone(['hiden','borderTg'])
+        } else {
+            setThirdZone(['hiden','borderTg'])
+        }
+    }
+    const onChoicedFourthZone = () => {
+        if (fourthZone[0]==='hiden') {
+            setFourthZone(['','borderActive'])
+
+            setFirstZone(['hiden','borderTg'])
+            setThirdZone(['hiden','borderTg'])
+            setSecondZone(['hiden','borderTg'])
+        } else {
+            setFourthZone(['hiden','borderTg'])
+        }
+    }
     const location = useLocation();
     const records = location.state.records;
     const age = location.state.age;
@@ -22,7 +70,7 @@ const TempoCalculatorResult = () => {
                 Введены неверные данные
                 <div>
                     <Link to="/tempocalculatorform">
-                        <Button>Открыть калькулятор темпов</Button>
+                        <Button>Ввести данные снова</Button>
                     </Link>
                 </div>
             </div>
@@ -64,33 +112,42 @@ const TempoCalculatorResult = () => {
                 </table>
             </div>
             <div className={"tempodata"}>
-                Базовая зона: 
+                <div className={firstZone[1]} onClick={onChoicedFirstZone}>
+                    Базовая зона: 
+                    <br />
+                    - рекомендуемый темп: {secToMin(easyTempoMax[0])}-{secToMin(easyTempoMin[1])}/км;
+                    <br />
+                    - рекомендуемый пульс:{HRRzones[0]}-{HRRzones[1]}уд/мин;
+                </div>
+                <p className={firstZone[0]}>Это ваши обычные пробежки. Большинство километров должно пробегаться именно с этими параметрами.</p>
                 <br />
-                - рекомендуемый темп: {secToMin(easyTempoMax[0])}-{secToMin(easyTempoMin[1])}/км;
+                <div className={secondZone[1]} onClick={onChoicedSecondZone}>
+                    Пороговая зона:
+                    <br />
+                    - рекомендуемый темп: {thresholdRun.tempoFast}-{thresholdRun.tempoSlow}/км;
+                    <br />
+                    - рекомендуемый пульс:{HRRzones[1]}-{HRRzones[2]}уд/мин;
+                </div>
+                <p className={secondZone[0]}>Отвечает за способность выводить лактат. Организм способен бежать в этой зоне 50-60 минут. Сильно влияет на результаты забегов от 10 до 21км. Определяется показателем анаэробного порога (<b>АНП</b>) </p>
                 <br />
-                - рекомендуемый пульс:{HRRzones[0]}-{HRRzones[1]}уд/мин;
-                <br /> <br />
-
-                Пороговая зона:
+                <div className={thirdZone[1]} onClick={onChoicedThirdZone}>
+                    Зона МПК:
+                    <br />
+                    - рекомендуемый темп: {intervalRun.tempoFast}-{intervalRun.tempoSlow}/км;
+                    <br />
+                    - рекомендуемый пульс:{HRRzones[2]}-{HRRzones[3]}уд/мин;
+                </div>
+                <p className={thirdZone[0]}>Эта зона отвечает за способность организма потреблять и превращать в энергию кислород. Проводя интервальные тренировки, направленные на повышение <b>МПК</b> Вы улучшите результаты в забегах длительностью 10-15 минут</p>
                 <br />
-                - рекомендуемый темп: {thresholdRun.tempoFast}-{thresholdRun.tempoSlow}/км;
-                <br />
-                - рекомендуемый пульс:{HRRzones[1]}-{HRRzones[2]}уд/мин;
-                <br /> <br />
-
-                Зона МПК:
-                <br />
-                - рекомендуемый темп: {intervalRun.tempoFast}-{intervalRun.tempoSlow}/км;
-                <br />
-                - рекомендуемый пульс:{HRRzones[2]}-{HRRzones[3]}уд/мин;
-                <br /> <br />
-
-                Зона повторов:
-                <br />
-                - рекомендуемый темп: {repeatRun.tempoFast}-{repeatRun.tempoSlow}/км;
-                <br />
-                - рекомендуемый пульс нет смысла отслеживать.
-                <br />
+                <div className={fourthZone[1]} onClick={onChoicedFourthZone}>
+                    Зона повторов:
+                    <br />
+                    - рекомендуемый темп: {repeatRun.tempoFast}-{repeatRun.tempoSlow}/км;
+                    <br />
+                    - рекомендуемый пульс нет смысла отслеживать.
+                    <br />
+                </div>
+                <p className={fourthZone[0]}>Тренировки в этой зоне повышают мощность и экономичность бега, так же влияют на результаты в гонках длительностью около 5 минут. Определяются показателем <b>МЩН</b></p>
             </div>
         </div> 
     );
